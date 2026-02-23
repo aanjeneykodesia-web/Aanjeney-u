6f7435bb9a0fc645186f872a1fc3a83a84a87022
aanjeneykodesia-web <aanjeneykodesia@gmail.com>
2026-02-23 14:49:24 +0530

    Update firebase1.js

--- js/firebase1.js
+++ js/firebase1.js
@@ -5,7 +5,7 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBOiQaZdLLjwGOo4Ag5nBOX-FaxzJH6f1w",
   authDomain: "fir-84f73.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
+  projectId: "fir-84f73",
   storageBucket: "YOUR.appspot.com",
   messagingSenderId: "ID",
   appId: "APP_ID"
