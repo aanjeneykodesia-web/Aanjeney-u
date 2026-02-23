085c7bd19a7f89e545bc8eb7a41f45a8e67653a1
aanjeneykodesia-web <aanjeneykodesia@gmail.com>
2026-02-23 10:58:45 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,16 +1,17 @@
-// firebase.js
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "DEMO_API_KEY_12345",
-  authDomain: "shopkeeper-demo.firebaseapp.com",
-  databaseURL: "https://shopkeeper-demo-default-rtdb.firebaseio.com",
-  projectId: "shopkeeper-demo",
-  storageBucket: "shopkeeper-demo.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:demo123abc"
+  apiKey: "AIzaSyBOiQaZdLLjwGOo4Ag5nBOX-FaxzJH6f1w",
+  authDomain: "fir-84f73.firebaseapp.com",
+  projectId: "fir-84f73",
+  storageBucket: "fir-84f73.firebasestorage.app",
+  messagingSenderId: "775433137122",
+  appId: "1:775433137122:web:de92016f28267de23be7bb"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
-const storage = firebase.storage();
+const app = initializeApp(firebaseConfig);
