0d07751316f571e0af0e56c68f7b6bb48d8599c8
aanjeneykodesia-web <aanjeneykodesia@gmail.com>
2026-02-24 22:29:35 +0530

    Delete js directory

--- js/dataManager.js
+++
@@ -1,7 +0,0 @@
-// js/dataManager.js
-import { db } from "./firebase.js";
-import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
-window.saveOrderToFirebase = async function(orderData) {
-  await addDoc(collection(db, "orders"), orderData);
-};
